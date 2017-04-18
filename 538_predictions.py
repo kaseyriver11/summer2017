980d56c0e4fcd1bd3b06f7fbb6d8ae6ba35e9ee6
kaseyriver <kaseyriver11@gmail.com>
2017-04-18 09:54:00 -0400

    Updating code for 538 predictions.

--- 538_predictions.py
+++ 538_predictions.py
@@ -8,6 +8,7 @@ Created on Fri Apr 14 16:24:11 2017
 
 import datetime as dt 
 import calendar
+import time
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -16,20 +17,60 @@ import pandas as pd
 ## Read Team Names
 team_names = pd.read_csv("data\\team_names.csv")
 
-
-url = "https://projects.fivethirtyeight.com/2017-mlb-predictions/games/"
-
-# Scrape the URL using BS
+                    ###                      ###
+                    ### --- TODAYS GAMES --- ###
+                    ###                      ###
+# What is today?
+month = dt.datetime.now().month
+if month < 10:
+    month = str(0) + str(month)
+# Create the URL and Scrap
+url = "http://m.mlb.com/schedule/2017/" + month + "/" + str(dt.datetime.now().day) + "/"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'lxml')
 
+## Matchups
+text = soup.find_all('td', {'class' : 'schedule-matchup text-left'})
+away = []; home= [];
+for i in range(0,len(text)):
+    matchup = text[i].getText().split()
+    for j in range(0,len(matchup)):
+        if matchup[j] == "@":
+            away.append(" ".join(matchup[0:j]))
+            home.append(" ".join(matchup[(j+1):len(matchup)]))
+
+## Times & Date
+text = soup.find_all('td', {'class' : 'schedule-time text-left'})
+times = []
+for i in range(0,len(text)):
+    value = text[i].getText()
+    value = value.replace("\n", "")
+    times.append(value)
+
+games = pd.DataFrame(columns=('Away', 'Home', 'Time'))
+games['Away'] = away; games['Home'] = home; games['Time'] = times
+
+## Grab Only Todays Games!
+daybreaks = []     
+for i in range(0,(len(away)-1)):
+    if times[i+1][0:times[i+1].index(':')] < times[i][0:times[i].index(':')]:
+        daybreaks.append(i+1)
+games = games[:daybreaks[0]]
+del(text, times, daybreaks, away, home, url, r, soup, i, j, matchup, month, value)
 
 
+                    ###                            ###
+                    ### --- TODAYS Predictions --- ###
+                    ###                            ###
+
+url = "https://projects.fivethirtyeight.com/2017-mlb-predictions/games/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'lxml')
 
 ## What is today?
-today = calendar.month_name[dt.datetime.now().month] + " " + str(dt.datetime.now().day)
-tomorrow = dt.date.today() + dt.timedelta(days=1)
-tomorrow = calendar.month_name[tomorrow.month] + " " + str(tomorrow.day)
+#today = calendar.month_name[dt.datetime.now().month] + " " + str(dt.datetime.now().day)
+#tomorrow = dt.date.today() + dt.timedelta(days=1)
+#tomorrow = calendar.month_name[tomorrow.month] + " " + str(tomorrow.day)
 
 # Grab the Team Names
 df = pd.DataFrame(columns=('Away', 'Home'))
@@ -59,8 +100,9 @@ for i in range(0,len(chance),2):
 df['Away_Chance'] = away 
 df['Home_Chance'] = home                  
 
+del(game, home, i, teams, url, words, away)
 
-  
+df = df[:games.shape[0]] 
   
   
 ### Scrap the Vegas Odds
@@ -91,18 +133,20 @@ for i in range(0, len(home)):
 df2 = pd.DataFrame(columns=('Away', 'Home', 'Away_Odds', 'Home_Odds'))
 df2['Away'] = away; df2['Home'] = home; df2['Away_Odds'] = away_odds;  df2['Home_Odds'] = home_odds
 
-#df = df[:df2.shape[0]]
-df = df[:13]
-df2 = df2[df2['Away'].isin(df['Away'])]
+#df2 = df2[df2['Away'].isin(df['Away'])]
 
 df = df.sort_values(by = ['Away'])
 df2 = df2.sort_values(by = ['Away'])
+games = games.sort_values(by = ['Away'])
 
 df = df.reset_index(drop=True)
 df2 = df2.reset_index(drop=True)
+games = games.reset_index(drop=True)
 
 df['Away_Odds'] = df2['Away_Odds']
 df['Home_Odds'] = df2['Home_Odds']
+df.insert(0, 'Date', time.strftime("%x")) 
+df.insert(1, 'Time', games['Time'])
 
 ## Away Numbers
 df['Away_Bet'] = 100; df['Away_Winnings'] = 0; df['Away_Projected_Winnings'] = 0;
@@ -127,9 +171,9 @@ df['Decision'] = ""
 for i in range(0,len(df['Decision'])):
     away_diff = (df['Away_Projected_Winnings'][i] - df['Away_Bet'][i])
     home_diff = (df['Home_Projected_Winnings'][i] - df['Home_Bet'][i])
-    if (away_diff > home_diff) & (away_diff > 0):
+    if (away_diff > home_diff) & (away_diff >= 5):
         df['Decision'][i] = "Away"
-    elif (home_diff > away_diff) & (home_diff > 0):
+    elif (home_diff > away_diff) & (home_diff >= 5):
         df['Decision'][i] = "Home"
     else:
         df['Decision'][i] = "None"
@@ -144,12 +188,12 @@ for i in range(0,len(df['Winnings'])):
         df['Bet'][i] = df['Home_Bet'][i]
         df['Winnings'][i] = df['Home_Projected_Winnings'][i]
 
-
+del(away, away_diff, away_odds, home, home_diff, i, j, odds, url)
 print(sum(df['Bet']))
 print(sum(df['Winnings']))
 
 
-
+df.to_csv("data\\winnings_4.14.2017.csv")
 
 
 
