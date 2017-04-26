4ea9c009c9dfee4acedd852e678021501f67f4c4
Will Burton <wjburton@ncsu.edu>
2017-04-26 13:10:35 -0400

    finish mlb daily script that connects and adds to AWS database

--- mlb_daily.py
+++ mlb_daily.py
@@ -11,7 +11,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import mysql.connector
+import fuzzywuzzy
 from fuzzywuzzy import process
+from sqlalchemy import create_engine
 
 cnx = mysql.connector.connect(host='baseball-db.ctxqwovrv2n6.us-east-1.rds.amazonaws.com',
                               user='baseball_proj',
@@ -19,7 +21,7 @@ cnx = mysql.connector.connect(host='baseball-db.ctxqwovrv2n6.us-east-1.rds.amazo
                               database='baseball_db')
 cursor = cnx.cursor()
 
-query = ("SELECT * FROM mlb_team_names")
+query = ("SELECT * FROM team_names")
 team_names_query = cursor.execute(query)
 team_names = pd.DataFrame(cursor.fetchall())
 team_names = team_names.rename(columns= {0:'team_id', 1:'team_name', 2:'city', 3:'team_abbr'})
@@ -63,10 +65,12 @@ def match_names(scraped_team_names, db_team_names):
 
 away = match_names(away, team_names.team_name)
 away = away.merge(team_names, on = 'team_name')
-away = away.team_abbr
+away['team_id'] = away['team_id'].astype(str)
+away = away.team_id
 home = match_names(home, team_names.team_name)
 home = home.merge(team_names, on = 'team_name')
-home = home.team_abbr
+home['team_id'] = home['team_id'].astype(str)
+home = home.team_id
 
 
 ## Times & Date
@@ -75,17 +79,20 @@ times = []
 for i in range(0,len(text)):
     value = text[i].getText()
     value = value.replace("\n", "")
-
     times.append(value)
 
-games = pd.DataFrame(columns=('time', 'away', 'home'))
-games['time'] = times; games['away'] = away; games['home'] = home
-games.insert(0, 'Date', time.strftime("%x")) 
-games.insert(0, 'ID', "")
+
+games = pd.DataFrame(columns=('time', 'away_team_id', 'home_team_id'))
+games['time'] = times; games['away_team_id'] = away; games['home_team_id'] = home
+games.insert(0, 'date', time.strftime("%Y-%m-%d"))
+games.insert(0, 'game_id', "")
 for i in range(0,len(times)):
-    times[i] = times[i][0:times[i].index(":")]
-games['id'] = month + str(day) + str(year) + "." + games['away'] + "." + games['home'] + "." + times
+    times[i] = times[i].replace(' ','')
+games['game_id'] = month + str(day) + str(year) + "." + games['away_team_id'] + \
+              "." + games['home_team_id'] + "." + times
 
-#del(text, times, away, home, url, r, soup, i, j, matchup, month, value, day, year, team_names)
+games = games[['game_id', 'home_team_id', 'away_team_id', 'date', 'time']]
 
-
+#insert the new game information into the database
+engine = create_engine('mysql+mysqlconnector://baseball_proj:NCSUMSA2017@baseball-db.ctxqwovrv2n6.us-east-1.rds.amazonaws.com:3306/baseball_db')
+games.to_sql(name='mlb_schedule', con=engine, if_exists = 'append', index=False)
